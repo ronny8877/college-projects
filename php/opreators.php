b277103e6bb4d266d8397febd7f9628aee2f2f62
Ronnie <sunnyrawat225@gmail.com>
2021-11-21 16:58:56 +0530

    almost done

---
+++ php/opreators.php
@@ -0,0 +1,19 @@
+<?php
+    $a=7;
+    $b=8;
+    $sum=$a+$b;
+    $sub=0;
+    if($a>$b)
+    $sub=$a-$b;
+    else
+    $sub=$b-$a;
+    $prod=$a*$b;
+    $quo=$a/$b;
+    $rem=$a%$b;
+    echo"The sum of numbers is:".$sum."\n";
+    echo"The difference of numbers is:".$sub."\n";
+    echo"The product of numbers is:".$prod."\n";
+    echo"The quotient of division of numbers is:".$quo."\n";
+    echo"The remainder of division of numbers is:".$rem."\n";
+    echo "\n";
+?>
